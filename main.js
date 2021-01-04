60f80a5abb531a3e02f00193262ad950cb02a52e
Ardian Glamniki <ardian.glamniki@hotmail.com>
2021-01-04 19:33:49 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+var container = document.getElementById("eventlist");
+var button = document.getElementById('btn');
+var pageCounter = 0;
+
+    button.addEventListener("click", function () {
+        $(document).ready(function() {
+            $.ajax({
+                url:"https://api.helsingborg.se/event/json/wp/v2/event",
+                headers: {"Accept": "application/json"}
+            })
+            .done(function(data){
+                list = $('events');
+                for (i = 0; i < data.length; i++) {
+                    html = '';
+                    html = '<li id="event_' + i + '">' + data[i]['name'] + '</li>';
+                    list.append(html);
+                    renderHTML(data[i]['title'].plain_text + data[i]['date'] + "<br>")
+                }
+                console.log(data);
+                button.style.visibility = "hidden";
+            })
+        });
+    });
+    dasd
+
+function renderHTML(data) {
+    container.insertAdjacentHTML('beforeend', data);
+}
